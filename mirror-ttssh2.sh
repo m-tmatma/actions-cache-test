c392f02475591d7745d0421ad8f28ae94ebc539c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-11-13 11:26:43 +0900

    check existence of svn repo

--- mirror-ttssh2.sh
+++ mirror-ttssh2.sh
@@ -4,12 +4,16 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 URL=file://$SCRIPT_DIR/ttssh2
 
 cd $SCRIPT_DIR
-svnadmin create ttssh2
+if [ ! -d ttssh2 ]; then
+    svnadmin create ttssh2
+
+    cd $SCRIPT_DIR/ttssh2
+    echo '#!/bin/sh'  > hooks/pre-revprop-change
+    echo 'exit 0'    >> hooks/pre-revprop-change
+    chmod +x            hooks/pre-revprop-change
+
+    svnsync init $URL http://svn.osdn.net/svnroot/ttssh2/
+fi
 
 cd $SCRIPT_DIR/ttssh2
-echo '#!/bin/sh'  > hooks/pre-revprop-change
-echo 'exit 0'    >> hooks/pre-revprop-change
-chmod +x            hooks/pre-revprop-change
-
-svnsync init $URL http://svn.osdn.net/svnroot/ttssh2/
 svnsync sync $URL
